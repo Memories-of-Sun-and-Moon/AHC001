283aa3600841b6e75fb9146394dd198e9404e578
mimas <univ.t1c2ha3@gmail.com>
2023-09-07 18:37:29 +0900

    add tester

---
+++ analysis_tools/archive.py
@@ -0,0 +1,18 @@
+import datetime
+import pathlib
+import subprocess
+
+def main(name: str):
+    pathlib.Path('archive').mkdir(exist_ok=True)
+
+    t_delta = datetime.timedelta(hours=9)
+    JST = datetime.timezone(t_delta, 'JST')
+    now = datetime.datetime.now(JST)
+    name = str(f'{now:%m%d%H%M}') + str(name)
+
+    pathlib.Path(name).mkdir()
+    
+    subprocess.run(['mv', '-v', 'out', f'{name}' + '/out'])
+    subprocess.run(['mv', '-v', f'{name}', 'archive/' + f'{name}'])
+    subprocess.run(['mv', 'score.txt', 'archive/'+ f'{name}' + '/score.txt'])
+    subprocess.run(['cp', 'AHC001.cpp', 'archive/' + f'{name}' + '/source.cpp'])
